888d62c97e125ce0f4f8e92881f0d381b528cb40
Bryant Novas <bryant.novas@gmail.com>
2021-04-24 18:20:06 -0400

    updates script.py to save charts

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
-import pandas as pd
 import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def scrape_and_save():
@@ -7,6 +8,28 @@ def scrape_and_save():
     html = pd.read_html(url, header=0, attrs={"class": "t-chart"})[0]
     with sqlite3.connect('db.sqlite3') as conn:
         html.to_sql('yields', conn, if_exists='replace')
+    plot_screenshot(html)
+
+
+def plot_screenshot(df):
+    df1 = df.iloc[:, 1:5]
+    df2 = df.iloc[:, 5:]
+
+    plt.plot(df1)
+    plt.title('Month based bonds yield change')
+    plt.legend(labels=df1.columns)
+    plt.ylabel('Interest %')
+    plt.xlabel('Day')
+
+    plt.savefig('./charts/monthly_yields.png')
+
+    plt.title('Year based bonds yield change')
+    plt.plot(df2)
+    plt.legend(labels=df2.columns)
+    plt.ylabel('Interest % ')
+    plt.xlabel('Day ')
+
+    plt.savefig('./charts/yearly_yields.png')
 
 
 if __name__ == '__main__':
